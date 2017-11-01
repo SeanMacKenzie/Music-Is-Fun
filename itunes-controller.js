d1653f98ed06682247cec6ecdd7b0f3ef353c863
Sean MacKenzie <sean.andrew.mackenzie@gmail.com>
2017-11-01 11:48:18 -0600

    prelunch push

--- itunes-controller.js
+++ itunes-controller.js
@@ -1,18 +1,37 @@
-function ItunesController(){
+function ItunesController() {
   var itunesService = new ItunesService()
   //Do Not Modify the getMusic function
-  this.getMusic = function getMusic(e){
+  this.getMusic = function getMusic(e) {
     e.preventDefault();
     var artist = e.target.artist.value;
     itunesService.getMusicByArtist(artist).then(draw); //after get music by artist returns what are you doing with the objects?
   }
 
   //Start coding here
-  function draw(){
+  function draw(songList) {
+    var template = ''
+    for (var i = 0; i < songList.length; i++) {
+      var result = songList[i];
+      template += `
+        <div class="col-md-4">
+          <img src="${result.albumArt}">
+          <h4>${result.artist}</h4>
+          <h6>${result.collection}</h6>
+          <h6>${result.preview}</h6>
+          <h4>${result.price}</h4>
+          <h6>${result.title}</h6>
+        </div>
+        `
+    }
+     document.getElementById('results').innerHTML = template 
   }
+      
 
 
 
 
-  
+
+
+
+
 }
