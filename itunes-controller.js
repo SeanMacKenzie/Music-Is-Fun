676442e9b9de627584c221e699617ac1fcfcbab3
Sean MacKenzie <sean.andrew.mackenzie@gmail.com>
2017-11-02 16:29:39 -0600

    Last push of 11/2 - rr

--- itunes-controller.js
+++ itunes-controller.js
@@ -13,31 +13,31 @@ function ItunesController() {
     var template = ''
     for (var i = 0; i < songList.length; i++) {
       var result = songList[i];
-      result.albumArt = result.albumArt.replace('100x100bb','1000x1000bb')
+      result.albumArt = result.albumArt.replace('100x100bb', '1000x1000bb')
+      
       template += `
-        <div class="col-md-6 border">
+        <div class="col-sm-6 border">
           <img src="${result.albumArt}">
           <h3>${result.artist}</h3>
           <h6>${result.collection}</h6>
           <h4>${result.title}</h4>
-          <audio controls="controls">
-            <source src="${result.preview}" />
-          </audio>
           <h6>${result.price}</h6>
-          
+          <audio controls="controls">
+            <source src="https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/Music3/v4/00/23/38/0023383d-1616-fbe2-b3a3-7f2de268911b/mzaf_7209253486009554987.plus.aac.p.m4a" />
+          </audio>
         </div>
         `
+        // I know the audio src isn't correct. At the bottom of the this .js file is the correct information to plug in. I couldn't resist a rick roll.
+
     }
     document.getElementById('results').innerHTML = template
   }
 
-  window.addEventListener("play", function(evt)
-  {
-      if(window.$_currentlyPlaying && window.$_currentlyPlaying != evt.target)
-      {
-          window.$_currentlyPlaying.pause();
-      } 
-      window.$_currentlyPlaying = evt.target;
+  window.addEventListener("play", function (evt) {
+    if (window.$_currentlyPlaying && window.$_currentlyPlaying != evt.target) {
+      window.$_currentlyPlaying.pause();
+    }
+    window.$_currentlyPlaying = evt.target;
   }, true);
 
   // document.addEventListener('play', function (e) {
@@ -49,7 +49,9 @@ function ItunesController() {
   //   }
   // }, true);
 
-
+  // <audio controls="controls">
+  //   <source src="${result.preview}" />
+  // </audio>
 
 
 
